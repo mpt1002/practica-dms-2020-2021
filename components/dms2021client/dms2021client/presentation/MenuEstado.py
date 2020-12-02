49660758f389638b6ece37c65c24dd9f76d33278
Maria <mpt1002@alu.ubu.es>
2020-12-02 12:13:48 +0100

    Reestructuración

--- components/dms2021client/dms2021client/presentation/MenuEstado.py
+++ components/dms2021client/dms2021client/presentation/MenuEstado.py
@@ -1,14 +1,20 @@
 
+from .ServiciosEstado import ServiciosEstado
+from ...bin.dms2021client import ManejadorPagina
 
+class MenuEstado(ServiciosEstado):
 
-class MenuEstado:
+    # Referncia a la clase contexto
+    __manejador : ManejadorPagina
 
-    def __init__(self, session_id: str):
-        pass
+    
+    def __init__(self, manejador: ManejadorPagina):
+        self.__manejador = manejador
 
-    @staticmethod
-    def ejecutarPagina(session_id: str) -> int:
+    #@staticmethod
+    def ejecutarPagina(self) -> int:
         opcion : int = 0
+        self.__manejador.set_opcion(opcion)
         while(opcion >= 0 and opcion < 6):
             print('MENU')
             print('\t1. Crear usuarios')
@@ -19,4 +25,8 @@ class MenuEstado:
             print('\t6. Salir')
             print("\tIntroduce una opcion (1-6): ")
             opcion = int(input())
+        self.__manejador.set_opcion(opcion)
         return opcion
+
+    def logout(self):
+        pass
