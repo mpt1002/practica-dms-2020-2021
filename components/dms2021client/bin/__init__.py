2e4b48b4ce0bac10fdc4972b487bcb39962bd540
Maria <mpt1002@alu.ubu.es>
2020-12-02 12:06:53 +0100

    Creado nuevo fichero para hacer nuevas referencias

---
+++ components/dms2021client/bin/__init__.py
@@ -0,0 +1,5 @@
+from dms2021client.data.config import ClientConfiguration
+from dms2021client.data.rest import AuthService
+from dms2021client.data.rest.exc import InvalidCredentialsError, UnauthorizedError
+from dms2021client.presentation import ServiciosEstado, MenuEstado
+from .dms2021client import ManejadorPagina
