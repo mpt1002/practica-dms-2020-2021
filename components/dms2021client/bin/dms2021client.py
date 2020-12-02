4e795cfb45b9fbe1edc0ed95aaa7147dbdcf973d
Maria <mpt1002@alu.ubu.es>
2020-12-02 12:11:55 +0100

    Renombrado de fichero

---
+++ components/dms2021client/bin/dms2021client.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+
+import time
+from getpass import getpass
+from dms2021client.data.config import ClientConfiguration
+from dms2021client.data.rest import AuthService
+from dms2021client.data.rest.exc import InvalidCredentialsError, UnauthorizedError
+from dms2021client.presentation import ServiciosEstado, MenuEstado
+
+class ManejadorPagina:
+
+    __estado : ServiciosEstado
+    __opcion : int
+    __session_id: str
+    __cfg: ClientConfiguration
+    __auth_service: AuthService
+
+    def __init__(self):
+        self.__cfg = ClientConfiguration()
+        self.__cfg.load_from_file(self.__cfg.default_config_file())
+        self.__auth_service = AuthService(self.__cfg.get_auth_service_host(), self.__cfg.get_auth_service_port())
+        #self.session_id : str = ""
+        self.__session_id = self.login()
+        
+        # Ir al estado inicial
+        self.__estado = MenuEstado(self)
+        '''
+        opcion = MenuEstado.ejecutarPagina(self.__session_id)
+        while opcion != 6:
+            if opcion == 0:
+                MenuEstado.ejecutarPagina(self.__session_id)
+            elif opcion == 1:
+                pass
+            elif opcion == 2:
+                pass
+            elif opcion == 3:
+                pass
+            elif opcion == 4:
+                pass
+            elif opcion == 5:
+                pass
+        self.logout()
+        '''
+    # Obtener el estado actual
+    def get_Estado(self) -> ServiciosEstado:
+        return self.__estado
+    
+    # Asignar un nuevo estado
+    def set_Estado(self, estado):
+        self.__estado = estado
+    
+    # Obtener la session id
+    def get_session_id(self) -> str:
+        return self.__session_id
+    
+    # Obtener la opcion actual
+    def get_opcion(self) -> int:
+        return self.__opcion
+
+    # Asignar una nueva opcion
+    def set_opcion(self, opcion: int) -> int:
+        self.__opcion = opcion
+
+    # Obtener la configuracion del cliente actual
+    def get_cfg(self) -> ClientConfiguration:
+        return self.__cfg
+    
+    # Obtener la autentificacion del servicio actual
+    def get_auth_service(self) -> AuthService:
+        return self.__auth_service
+
+    def login(self) -> str:
+        
+        while not self.__auth_service.is_running():
+            time.sleep(1)
+        print("\nAuthentication service up!")
+        
+        
+        print('Press Ctrl+C to end this process.')
+        print('If run as a docker container, press Ctrl+P,Ctrl+Q to detach from the container.')
+        username: str = input('Username: ')
+        password: str = getpass('Password: ')
+        try:
+            self.__session_id = self.__auth_service.login(username, password)
+            print('Logged in successfully as ' + username + ' . Session id: ' + self.__session_id)
+        except InvalidCredentialsError:
+            print('Wrong username and/or password. Try again.')
+        except Exception as ex:
+            print(ex)
+
+        return self.__session_id
+
+ManejadorPagina()
+# Crear un metodo main para la ejecucion de todo con el switch
