0443727c6f9f8596cfc06c35da90088a896e6be6
Maria <mpt1002@alu.ubu.es>
2020-12-02 12:16:46 +0100

    Creacion de un nuevo estado concreto

---
+++ components/dms2021client/dms2021client/presentation/ExitEstado.py
@@ -0,0 +1,24 @@
+from dms2021client.data.rest.exc import InvalidCredentialsError, UnauthorizedError
+from .ServiciosEstado import ServiciosEstado
+from ...bin.dms2021client import ManejadorPagina
+
+class ExitEstado(ServiciosEstado):
+
+    # Referncia a la clase contexto
+    __manejador : ManejadorPagina
+
+    def __init__(self, manejador: ManejadorPagina):
+        self.__manejador = manejador
+
+    def ejecutarPagina(self):
+        pass
+
+    def logout(self):
+
+        try:
+            self.__manejador.get_auth_service().logout(self.__manejador.get_session_id())
+            print('Logged out successfully.')
+        except UnauthorizedError:
+            print('Wrong session id.')
+        except Exception as ex:
+            print(ex)
