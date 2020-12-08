678b03179d24d7b9932737eb1b237cd135cefb22
Maria <mpt1002@alu.ubu.es>
2020-12-08 10:16:45 +0100

    Entrega 1: Solucionar errores de ejecución

--- components/dms2021sensor/dms2021sensor/logic/rest/restSensor.py
+++ components/dms2021sensor/dms2021sensor/logic/rest/restSensor.py
@@ -2,16 +2,22 @@ from dms2021core.data.rest import RestResponse
 from dms2021sensor.data import Sensor, SensorFile
 
 class RestSensor():
-    __sensores : {}
+    __sensores = {}
 
     def __init__(self):
-        self.__sensores['sensor1'] = SensorFile('FicheroABuscar.txt')
+        self.__sensores['sensor1'] = SensorFile('ficheroABuscar.txt')
 
     def obtenerRespuestaSensor(self, nombreSensor:str) -> RestResponse:
-        if nombreSensor in self.__sensores:
+        keys = self.__sensores.keys()
+        flag = False
+        for s in keys:
+            if nombreSensor == s:
+                flag = True
+                break
+
+        if flag:
             valorSensor = str(self.__sensores[nombreSensor].monitorizar())
-            RestResponse(valorSensor, 200, 'text/plain')
-            return RestResponse
+            return RestResponse(valorSensor, 200, 'text/plain')
         else:
             #Si no existe el sensor ERROR 404
             return RestResponse(code = 404, mime_type = 'text/plain')
