efbc837e4e132a53ebeaede810f66189d2f629f5
Maria <mpt1002@alu.ubu.es>
2020-12-28 10:03:37 +0100

    Entrega 2: Implementación del nuevo sensor "SensorSystem"

---
+++ components/dms2021sensor/dms2021sensor/data/sensorSystem.py
@@ -0,0 +1,23 @@
+from .Sensor import Sensor
+import os
+
+class SensorSystem(Sensor):
+    __parametros:str
+    __porcentaje:int
+
+    def __init__(self, parametros:str, porcentaje:int = 50):
+        # parametros, que caracteristicas del sistema se quiere monitorizar
+        self.__parametros = parametros
+        self.__porcentaje = porcentaje
+
+    def monitorizar(self):
+        # Monitorizar solo Mem y Swap
+        proceso = os.popen('cat /proc/meminfo|grep "'+self.__parametros+'"').read()
+        memTotal:str = os.popen('cat /proc/meminfo|grep "'+self.__parametros+'Total"|tr -s " " ":"|cut -d ":" -f 2 ').read()
+        memFree:str = os.popen('cat /proc/meminfo|grep "'+self.__parametros+'Free"|tr -s " " ":"|cut -d ":" -f 2 ').read()
+        dif:float = float(memTotal) - float(memFree)
+        
+        if dif > float(memTotal)*(self.__porcentaje/100):
+            return proceso + "La "+self.__parametros+" usada supera el "+str(self.__porcentaje)+"%\n"
+        else:
+            return proceso + "La "+self.__parametros+" usada NO supera el "+str(+self.__porcentaje)+"%\n"
