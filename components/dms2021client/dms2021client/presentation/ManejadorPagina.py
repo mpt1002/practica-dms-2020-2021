d9b5ceaa88f96f3afec5360952a89cf7f875b346
Marckimarck <mri1001@alu.ubu.es>
2021-01-16 11:10:09 +0100

    Entrega 2: Actualización de ManejadorPagina

--- components/dms2021client/dms2021client/presentation/ManejadorPagina.py
+++ components/dms2021client/dms2021client/presentation/ManejadorPagina.py
@@ -7,47 +7,45 @@ from .loginEstado import LoginEstado
 
 class ManejadorPagina:
 
-    __estado : ServiciosEstado
-    __opcion : int
-    __session_id: str
-    __username : str
-    __cfg: ClientConfiguration
-    __auth_service: AuthService
-    __sensor_service : SensorService
-
     def __init__(self):
         print('Bienvenido\nEl programa se esta inicializando')
-        self.__cfg = ClientConfiguration()
+        self.__cfg: ClientConfiguration = ClientConfiguration()
         self.__cfg.load_from_file(self.__cfg.default_config_file())
-        self.__auth_service = AuthService(self.__cfg.get_auth_service_host(), self.__cfg.get_auth_service_port())
-        self.__estado = LoginEstado(self.__auth_service)
-        opcion: int = self.__estado.ejecutarPagina()
-        self.__session_id = self.__estado.get_session_id()
-        self.__username = self.__estado.get_username()
-        
+        self.__auth_service: AuthService = AuthService(self.__cfg.get_auth_service_host(), self.__cfg.get_auth_service_port())
+        self.__estado: ServiciosEstado = LoginEstado(self.__auth_service)
+        self.__opcion: int = self.__estado.ejecutarPagina()
+        self.__session_id: str = self.__estado.get_session_id()
+        self.__username: str = self.__estado.get_username()
+        self.__sensor_service: SensorService = None
+                                
+
+    def ejecutar_servicio(self):
         while True:
-            if opcion == 0:
+            if self.__opcion == 0:
                 self.__estado = MenuEstado(self.__session_id)
-            elif opcion == 1:
+            elif self.__opcion == 1:
                 self.__estado = CrearUsuariosEstado(self.__session_id, self.__username, self.__auth_service)
-            elif opcion == 2:
+            elif self.__opcion == 2:
                 self.__estado = ModificarPermisosEstado(self.__session_id, self.__username, self.__auth_service)
-            elif opcion == 3:
+            elif self.__opcion == 3:
                 self.__sensor_service = self.__get_sensor_service()
                 self.__estado = GestionarSensoresEstado(self.__session_id, self.__username, self.__auth_service, self.__sensor_service)
-            elif opcion == 4:
+            elif self.__opcion == 4:
                 self.__sensor_service = self.__get_sensor_service()
                 self.__estado = AjusteSensoresEstado(self.__session_id, self.__username, self.__auth_service, self.__sensor_service)
-            elif opcion == 5:
+            elif self.__opcion == 5:
                 self.__sensor_service = self.__get_sensor_service()
                 self.__estado = MonitorizarSensoresEstado(self.__username, self.__auth_service, self.__sensor_service)
-            elif opcion == 6:
+            elif self.__opcion == 6:
                 self.__estado = ExitEstado(self.__session_id, self.__auth_service)
-            elif opcion == 7:
+            elif self.__opcion == 7:
                 #La opcion 7 solo saltara si se ha hecho el correcto logout
                 break
-            opcion = self.__estado.ejecutarPagina()
-                                
+            self.__ejecutarPagina()
+
+    def __ejecutarPagina(self):
+        self.__opcion = self.__estado.ejecutarPagina()
+
     # Obtener el estado actual
     def get_Estado(self) -> ServiciosEstado:
         return self.__estado
@@ -63,11 +61,6 @@ class ManejadorPagina:
     # Obtener la opcion actual
     def get_opcion(self) -> int:
         return self.__opcion
-
-    # Asignar una nueva opcion
-    def set_opcion(self, opcion: int) -> int:
-        self.__opcion = opcion
-        return opcion
 
     # Obtener la configuracion del cliente actual
     def get_cfg(self) -> ClientConfiguration:
